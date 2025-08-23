869aa36fd3c2a75b95cc3ce180156fe4d55efa2c
Ajay <gangwar030@gmail.com>
2025-08-23 23:43:13 +0530

    Update java

---
+++ Basic+core/update.java
@@ -0,0 +1,15 @@
+
+// Java update came in java 21 that is below...
+// Now we can Run Java files Without class and psvm method
+
+void main() 
+{
+    System.out.println("Hello World update in Java 21");
+}
+
+// public class update {
+//     public static void main(String[] args) {
+        
+//         System.out.println("Hello World old");
+//     }
+// }
