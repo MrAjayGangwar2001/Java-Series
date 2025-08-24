9334af6120a389fa77f1066d36a5a16b388db1ae
Ajay <gangwar030@gmail.com>
2025-08-24 20:39:55 +0530

    Simple Test File

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        for(; i<=10; i=i+2){
+            System.out.println(i);
+        }
+        System.out.println("This is test : " +i);
+    }
+}
