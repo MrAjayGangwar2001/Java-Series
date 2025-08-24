dc616f0f58e92acb511fe3f3fba1591c188fb58f
Ajay <gangwar030@gmail.com>
2025-08-24 20:46:40 +0530

    Simple Loop File

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+// This is Normal Loop File
+
 public class Main {
     public static void main(String[] args) {
         int i = 0;
         for(; i<=10; i=i+2){
-            System.out.println(i);
+            System.out.println("This will print all even Number from 0 to 10 : "+i);
         }
         System.out.println("This is Testing for Loop in Java: ");
         System.out.println("This Will print i value End of the loop : " +i);
