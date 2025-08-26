931f5eea79d7699bb7dc862e3865aea55e89e41e
Ajay <gangwar030@gmail.com>
2025-08-26 17:09:47 +0530

    StringBuffer

--- Basic+core/StringBufferMethod.java
+++ Basic+core/StringBufferMethod.java
@@ -4,7 +4,7 @@ public class StringBufferMethod{
     public static void main(String[] args) {
         
         StringBuffer sb = new StringBuffer("Ajay");
-        sb.append(" Gangwar");
+        sb.append(" Gangwar   ");
         int ab = sb.capacity();  // 20
         sb.deleteCharAt(0) ;
         System.out.println(sb);  // Gangwar
@@ -13,7 +13,6 @@ public class StringBufferMethod{
         sb.insert(3 , "A");
         System.out.println(sb);
         System.out.println(ab);
-         
     }
 
 }
