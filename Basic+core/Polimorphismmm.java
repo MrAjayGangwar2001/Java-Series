9239ade2e2f547712daf4c1c1e730ca7f2fc0a28
Ajay <gangwar030@gmail.com>
2025-08-29 01:19:20 +0530

    Polymorphism

---
+++ Basic+core/Polimorphismmm.java
@@ -0,0 +1,35 @@
+//😊😊😊😊😊😊😊😊😊👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉--------POLYMORPHISM-----😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊
+
+//  Polymorphism means the ability of one thing to behave in different ways depending on the situation.
+
+/// This Is Example of Run Time Polymorphism
+class AA {
+    public void show() {
+        System.out.println("Class A in Show Method");
+    }
+
+}
+class BB extends AA{
+    public void show()
+    {
+        System.out.println("Class B in show Method");
+    }
+}
+class CC extends AA{
+    public void show()
+    {
+        System.out.println("Class C in show Method");
+    }
+}
+
+public class Polimorphismmm {
+    public static void main(String[] args) {
+        AA obj = new AA();
+        obj.show();
+
+        // obj = new BB();
+        // obj.show();
+        // obj = new CC();
+        // obj.show();
+    }
+}
