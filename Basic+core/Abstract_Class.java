c6503dd1135acd7287dcc4e3a1f8350eedfd2d1a
Ajay <gangwar030@gmail.com>
2025-09-02 21:39:39 +0530

    AbstractOne

---
+++ Basic+core/Abstract_Class.java
@@ -0,0 +1,45 @@
+//   ----------------------ABSTRACT CLASS----------------------
+//  Abstract class must have an Abstract Method
+//  if we applying abstract method it required to make abstract method
+
+
+
+abstract class Car {
+    public abstract  void drive();            // Abstract method
+    public abstract void ferrari();           // Abstract method
+
+    public void playmusic() {
+        System.out.println("It is musicplaying");
+    }
+}
+
+abstract class BrandedCar extends Car {
+    public void drive() {
+        System.out.println("It is a BMW car");
+    }
+    public void mercedeceBenz() {
+        System.out.println("It is a Mercedece car");
+    }
+}
+class worldTopCar extends BrandedCar {
+    public void ferrari()
+    {
+        System.out.println("It is Rare Car");
+    }
+    public void royalsroys()
+    {
+        System.out.println("It is Royalsroys Car");
+    }
+}
+
+
+public class Abstract_Class {
+    public static void main(String[] args) {
+        Car obj = new worldTopCar();
+        obj.drive();
+        obj.ferrari();
+        obj.playmusic();
+
+    }
+
+}
