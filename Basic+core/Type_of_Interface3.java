f30f99d0c10af1255f27cffd798f87cb6650fd43
Ajay <gangwar030@gmail.com>
2025-09-07 23:32:37 +0530

    InterfaceType

--- Basic+core/Type_of_Interface3.java
+++ Basic+core/Type_of_Interface3.java
@@ -1,7 +1,8 @@
 // Ther Are Three Types of Interface
 // 1. Normal Interface :-
 //                           if we have 2 or more Method that is Normal interface 
-
+// //  Functional/SAM Interface :- 
+//                                 if We have one Method Interface that's called SAM(Single Abstract Method);;
 
 // Functional interface
 @FunctionalInterface
