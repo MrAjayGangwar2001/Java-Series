993d764ebc1973630a557fe491f87ee1813fa1a6
Ajay <gangwar030@gmail.com>
2025-09-08 20:08:31 +0530

    EnumOne

---
+++ Basic+core/Enum1.java
@@ -0,0 +1,21 @@
+
+
+// Enum is a method which used as Constant to provide the status of the process
+ 
+
+
+enum Status{
+    Running , Failed , Pending , Success
+}
+
+public class Enum1 {
+    public static void main(String[] args) {
+        Status s = Status.Success;
+        System.out.println(s);
+        // Other Way to use it
+        Status[] ss = Status.values();
+        System.out.println(ss[1]);
+        
+       
+    }
+}
