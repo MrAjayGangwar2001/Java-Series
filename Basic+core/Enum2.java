0113c2e14de8231e65f2672b4f12f037e8e65a7a
Ajay <gangwar030@gmail.com>
2025-09-09 23:24:23 +0530

    EnumTwo

---
+++ Basic+core/Enum2.java
@@ -0,0 +1,29 @@
+
+enum Laptop{
+    Macbook(90000), HP(70000) , Dell(60000) , Lenovo(50000);
+
+    private int price;
+
+    private Laptop(int price){
+        this.price = price;
+        
+    }
+    public int  getPrice(){
+        return price;
+    }
+    public void setPrice(int price){
+        this.price = price;
+         
+    };
+}
+
+public class Enum2 {
+    public static void main(String[] args) {
+       
+
+        for(Laptop lap : Laptop.values())
+        {
+            System.out.println(lap+ " : " +lap.getPrice());
+        }
+    }
+}
