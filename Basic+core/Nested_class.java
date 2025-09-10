6421d92be5846e28cf336535b0ca27cdea7825b1
Ajay <gangwar030@gmail.com>
2025-09-10 22:49:52 +0530

    NestedClass

---
+++ Basic+core/Nested_class.java
@@ -0,0 +1,41 @@
+
+class NA {
+    int n;
+
+    public void show() {
+        System.out.println("class A in show");
+
+    }
+
+    // static class N_B {
+    // // public void Ba() {
+    // // System.out.println("Nested Show");
+    // // }
+    // }
+}
+
+public class Nested_class {
+    public static void main(String[] args) {
+        /*
+        NA obj = new NA();
+        obj.show();
+         
+
+        //NA.N_B obj1 = obj.new N_B();    // we refered class NA to N_B
+        NA.N_B obj1 = new NA.N_B();
+        obj1.Ba();
+         */
+
+
+         // 👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇--------NEW METHOD TO USE METHOD-----👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
+
+         NA obj = new NA()
+         {
+            public void show() {
+                 
+               System.out.println("This is new method/way to use Method");
+           }
+         };
+         obj.show();
+    }
+}
