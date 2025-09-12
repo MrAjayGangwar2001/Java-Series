5b627883f76c60978bdcef4958b2129e302ff9e7
Ajay <gangwar030@gmail.com>
2025-09-13 00:28:40 +0530

    LambdaFileOne

--- Basic+core/Lambda_Expression1.java
+++ Basic+core/Lambda_Expression1.java
@@ -24,12 +24,12 @@ interface X {
 public class Lambda_Expression1 {
 
     public static void main(String[] args) {
-        // X obj = new X() {                   // No need to Write These line
+        // X obj = new X() {                   // No need to Write These line of Code . It requir main code
         // public void display() {
         // System.out.println("This is Functional Interface");
         // }
         // };
-        X obj = () -> System.out.println("This is Printed By Lamda Expression");
+        X obj = () -> System.out.println("This is Printed By Lamda Expression without writing Anonymous class");
         obj.display();
     }
 ;
