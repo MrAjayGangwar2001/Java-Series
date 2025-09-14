116af2eae19c5988c34a6b8f61e8db45a12876ad
Ajay <gangwar030@gmail.com>
2025-09-14 23:21:59 +0530

    LambdaExpThree

---
+++ Basic+core/Lamda_Expression3.java
@@ -0,0 +1,27 @@
+//Lamda expression part 3
+
+
+@FunctionalInterface
+interface Aj{
+    int add(int x,int y);
+}
+
+public class Lamda_Expression3 {
+    public static void main(String[] args) {
+        // BASIC AND OLD METHOD 🤦‍♂️🤦‍♂️🤦‍♂️🤦‍♂️🤦‍♂️
+        
+        Aj obj = new Aj()
+        {
+            public int add(int x, int y)
+            {
+                return x+y;
+            }
+        };
+        int sum = obj.add(6,9);
+        System.out.println(sum);
+
+       
+        int sum = obj.add(6,9);
+        System.out.println(sum);
+    }
+}
