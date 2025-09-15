b66e32c85381b9c20615eddd431260a5a808fa30
Ajay <gangwar030@gmail.com>
2025-09-15 18:05:56 +0530

    Annotation

---
+++ Basic+core/Annotation.java
@@ -0,0 +1,21 @@
+
+class PCM {
+    public void UGkebaadsakopaisakamanahai()
+    {
+        System.out.println("Physics , Chemistry , Math");
+    }
+}
+
+class ZBC extends PCM {
+
+    public void UGkebaadsakopaisakamanahai() {
+        System.out.println("Zology , Botany , Chemistry");
+    }
+}
+
+public class Annotation {
+    public static void main(String[] args) {
+        ZBC paisa = new ZBC();
+        paisa.UGkebaadsakopaisakamanahai();
+    }
+}
