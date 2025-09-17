74a1bfba545532fc5cd57c3fba1ab659f42408ba
Ajay <gangwar030@gmail.com>
2025-09-18 01:47:10 +0530

    DefaultConstructor

--- Basic+core/Def_const.java
+++ Basic+core/Def_const.java
@@ -1,4 +1,5 @@
 // Here In this file You will see the Default Constructor In java
+// Default Constructor Means We doed not pass the Parameter during the Method Creation or in Method
     class Student {
     Student() {
         System.out.println("This is a default constructor.");
