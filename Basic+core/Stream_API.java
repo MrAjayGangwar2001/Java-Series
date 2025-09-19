e1e784dbf2f4c9992ab0d1dd33fd0b077e2f8230
Ajay <gangwar030@gmail.com>
2025-09-19 23:38:28 +0530

    StreamAPI

--- Basic+core/Stream_API.java
+++ Basic+core/Stream_API.java
@@ -2,7 +2,8 @@
 
 // Streams Provides Lot of Methods Like filter , map, reduce, sorted, find, findIndex, forEach, some, every, etc.
 // Once we print the stream it will be executed and it will be closed.again we can't Re-print the same value by the same stream.
-
+// Streams Comes from Interface
+//  In the Stream API Methods we Used Many Methods to Print List. We will See below :-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
