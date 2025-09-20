9db77a13281ee917bbabcf1cbce2fcdb2a4e044d
Ajay <gangwar030@gmail.com>
2025-09-21 00:30:26 +0530

    Main class file

---
+++ Basic+core/Inheritance/Main.java
@@ -0,0 +1,15 @@
+
+
+//  ****** Like this we can use another file and make it chain like child ******//
+
+public class Main {
+    public static void main(String[] args) {
+        VeryAdvCalc obj = new VeryAdvCalc();
+        int r1 = obj.add(5 , 7);
+        int r2 = obj.sub(15 , 7);
+        int r3 = obj.mul(5 , 7);
+        int r4 = obj.div(15 , 3);
+        double r5 = obj.pow(2 , 5);
+        System.out.println(r1+ " : " +r2+ " : " +r3+ " : " +r4+ ": "+r5);
+    }
+}
