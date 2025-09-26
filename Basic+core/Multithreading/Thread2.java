eec8e4373e0cfc6aa63acdfef095cb043727176d
Ajay <gangwar030@gmail.com>
2025-09-26 16:28:20 +0530

    ThreadFileTwo

--- Basic+core/Multithreading/Thread2.java
+++ Basic+core/Multithreading/Thread2.java
@@ -2,17 +2,17 @@
 /// THIS IS THE EXAMEOLE OF IMPLEMENTS RUNNABLE----TO EXTENDS---
 
 class A implements Runnable {
-    public void run() {
-    for (int i = 0; i < 10; i++) {
-    System.out.println("Ajay");
+    // public void run() {
+    // for (int i = 0; i < 10; i++) {
+    // System.out.println("Ajay");
 
-    try {
-    Thread.sleep(1);
-    } catch (InterruptedException e) {
-    System.out.println("It's Done! ");
-    }
-    }
-    }
+    // try {
+    // Thread.sleep(1);
+    // } catch (InterruptedException e) {
+    // System.out.println("It's Done! ");
+    // }
+    // }
+    // }
 }
 
 class B implements Runnable {
@@ -31,7 +31,20 @@ class B implements Runnable {
 public class Thread2 {
     public static void main(String args[]) {
 
-        Runnable obj1 = new Runnable();    
+       // Runnable obj1 = new Runnable();    // or
+        Runnable obj1 = () -> {                              // Runnable also a functional Interface that's why we can use Lamda Interface
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Ajay");
+
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    System.out.println("It's Done! ");
+                }
+            }
+            
+        };
+
         
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
