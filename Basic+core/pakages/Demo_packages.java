8f61a07faf5ca3506ed53471e702b34311c39201
Ajay <gangwar030@gmail.com>
2025-09-28 02:03:52 +0530

    PackagesInJava

--- Basic+core/pakages/Demo_packages.java
+++ Basic+core/pakages/Demo_packages.java
@@ -3,15 +3,15 @@
 
 public class Demo_packages {
     public static void main(String[] args) {
+        System.out.println("
+        // Package is a structured of Files and Folder
+        //  Package is to manage files in Folder Structure
+        //  Package is Useful to Import file from Another Folder by Using import (package name).(file name) 
         
+        //  ex. import tool.Calc; // here we used package 
+        
+        // I Used Package in Method Overridding1 and 2.
+        //   also imported file from others
+        ");
     }
 }
-
-// Package is a structured of Files and Folder
-//  Package is to manage files in Folder Structure
-//  Package is Useful to Import file from Another Folder by Using import (package name).(file name) 
-
-//  ex. import tool.Calc; // here we used package 
-
-// I Used Package in Method Overridding1 and 2.
-//   also imported file from others
