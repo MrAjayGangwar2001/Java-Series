0a56f8ad193b2f8e02091e3a4c8e6d7431008693
Ajay <gangwar030@gmail.com>
2025-10-03 20:25:41 +0530

    LinkedList

---
+++ Basic+core/Collection/LinkList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class LinkList {
+    public static void main(String[] args) {
+        
+
+        List<Integer> ls = new LinkedList<Integer>();
+
+        ls.add(10);
+        ls.add(20);
+        ls.add(30);
+        ls.add(40);
+        ls.add(3, 35);
+        ls.add(50);
+        ls.add(45);
+        ls.add(45);
+       
+        System.out.println("List Of The Elements " + ls);
+
+    }
+}
