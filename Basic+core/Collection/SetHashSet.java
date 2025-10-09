d9ad9f1e06e9380152d93b0d6ce87affcebae6b3
Ajay <gangwar030@gmail.com>
2025-10-09 12:23:50 +0530

    HashSet

---
+++ Basic+core/Collection/SetHashSet.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/
+public class SetHashSet {
+    public static void main(String[] args) {
+        
+
+       Set<Integer> st = new HashSet<Integer>();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        st.add(5);
+        st.add(4);
+        
+
+       System.out.println(st);
+    }
+}
