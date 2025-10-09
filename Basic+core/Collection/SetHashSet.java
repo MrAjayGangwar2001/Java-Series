a23f479fc1dc24903190bd00898c62260fa7d0f7
Ajay <gangwar030@gmail.com>
2025-10-09 12:27:07 +0530

    HashSet

--- Basic+core/Collection/SetHashSet.java
+++ Basic+core/Collection/SetHashSet.java
@@ -6,23 +6,35 @@ public class SetHashSet {
     public static void main(String[] args) {
         
 
-       Set<Integer> st = new HashSet<Integer>();
-        st.add(5);
-        st.add(1);
-        st.add(2);
-        st.add(3);
-        st.add(5);
-        st.add(4);
-        st.add(0);
-        st.add(56);
-        st.add(7);
-        st.add(28);
-        st.add(9);
-        st.add(30);
+      //  Set<Integer> st = new HashSet<Integer>();
 
-        st.add(-10);
+      
+      //   st.add(5);
+      //   st.add(1);
+      //   st.add(2);
+      //   st.add(3);
+      //   st.add(5);
+      //   st.add(4);
+      //   st.add(0);
+      //   st.add(56);
+      //   st.add(7);
+      //   st.add(28);
+      //   st.add(9);
+      //   st.add(30);
+      
+      //   st.add(-10);
+      
+      
+      //  System.out.println(st);
 
 
-       System.out.println(st);
+      Set<String> Shop = new HashSet<>();
+
+
+        Shop.add("Car");
+        Shop.add("bike");
+        Shop.add("Car");
+        Shop.add("bike");
+        Shop.add("Car");
     }
 }
