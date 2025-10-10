309cad65ef3f75d4679ea93d85c81f3c2e398297
Ajay <gangwar030@gmail.com>
2025-10-11 00:24:39 +0530

    Stack

---
+++ Basic+core/Collection/Stackk.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class Stackk {
+    public static void main(String[] args) {
+
+        Stack<Integer> sk = new Stack<>();
+
+        sk.add(101); {
+        sk.add(102); // These Methods are deprecated OR
+        sk.add(103); // It is Valid Because
+        sk.add(104); // Vector Extends Stack So
+       
+
+        System.out.println(sk);
+    }
+}
