d843b320b9ca9fbd024ad762fb47c09c0836788b
Ajay <gangwar030@gmail.com>
2025-10-11 00:33:03 +0530

    Vector

--- Basic+core/Collection/Vectors.java
+++ Basic+core/Collection/Vectors.java
@@ -1,17 +1,20 @@
 import java.util.Enumeration;
-import java.util.Vector; 
-
+import java.util.Vector;
 
 //  Vector is a Synchronized so it is slow and thread safe
 public class Vectors {
-    public static void main(String[] args) {
-        
+  public static void main(String[] args) {
 
-      Vector<String> vc = new Vector<String>();
-      vc.add("am");
-      vc.add("Ajay");
-      vc.add("Gangwar");
-     
-        System.out.println(vc);
-    }
+    Vector<String> vc = new Vector<String>();
+    vc.add("am");
+    vc.add("Ajay");
+    vc.add("Gangwar");
+    System.out.println(vc);
+    vc.add(0, "I");
+
+
+
+    
+    System.out.println(vc);
+  }
 }
