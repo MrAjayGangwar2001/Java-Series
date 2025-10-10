bf6c7332d2acbebd0b8ac5e97c97f9738dbde4d0
Ajay <gangwar030@gmail.com>
2025-10-11 00:27:40 +0530

    Stack

--- Basic+core/Collection/Sorting.java
+++ Basic+core/Collection/Sorting.java
@@ -24,62 +24,68 @@ public class Sorting {
 
     public static void main(String[] args) {
         /*
-        // We are sorting numsList values basis on second or last digit like index 1
-        Comparator<Integer> com = new Comparator<Integer>()
-        {
-            public int compare(Integer i , Integer j)
-            {
-                if (i%10 > j%10) {
-                    return 1;             //  return 1 means it swap nums on condition
-                }
-                else {
-                    return -1;            // return -1 means it don't swap nums on condition
-                }
-            }
-        };
-        
-        List<Integer> numsList = new ArrayList<>();
-        numsList.add(51);
-        numsList.add(65);
-        numsList.add(79);
-        numsList.add(93);
-        numsList.add(37);
-        numsList.add(46);
-        numsList.add(52);
-
-        Collections.sort(numsList);
-        Collections.sort(numsList, com);
-
-        System.out.println(numsList);  */
+         * // We are sorting numsList values basis on second or last digit like index 1
+         * Comparator<Integer> com = new Comparator<Integer>()
+         * {
+         * public int compare(Integer i , Integer j)
+         * {
+         * if (i%10 > j%10) {
+         * return 1; // return 1 means it swap nums on condition
+         * }
+         * else {
+         * return -1; // return -1 means it don't swap nums on condition
+         * }
+         * }
+         * };
+         * 
+         * List<Integer> numsList = new ArrayList<>();
+         * numsList.add(51);
+         * numsList.add(65);
+         * numsList.add(79);
+         * numsList.add(93);
+         * numsList.add(37);
+         * numsList.add(46);
+         * numsList.add(52);
+         * 
+         * Collections.sort(numsList);
+         * Collections.sort(numsList, com);
+         * 
+         * System.out.println(numsList);
+         */
 
         // Now we starts Students class and sorting of students basis on age
-        // 
-        //     Comparator<Students> com = new Comparator<Students>()  /// Comaprator is a Functional Interface
-        //   {
-        //         public int compare(Students i , Students j)
-        //         {
-        //             if (i.age > j.age) {
-        //                 return 1;             //  return 1 means it swap nums on condition
-        //             }
-        //             else {
-        //                 return -1;            // return -1 means it don't swap nums on condition
-        //             }
-        //         }
-        //     }; 
+        //
+        // Comparator<Students> com = new Comparator<Students>() /// Comaprator is a
+        // Functional Interface
+        // {
+        // public int compare(Students i , Students j)
+        // {
+        // if (i.age > j.age) {
+        // return 1; // return 1 means it swap nums on condition
+        // }
+        // else {
+        // return -1; // return -1 means it don't swap nums on condition
+        // }
+        // }
+        // };
         // We are Using Lamda Expression to sort students basis on age
-        Comparator<Students> com = (i, j) -> i.age > j.age ? 1 : -1;  // we can use this type of  Lamda Expression in one line or Below the code of both types
-       /*  Comparator<Students> com = (i, j)
-                -> {
-                    return i.age > j.age ? 1 : -1;
-
-               // 🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️ [Instead of if else we can Use Ternory Operator]
-            // if (i.age > j.age) {       
-            //     return 1;             //  return 1 means it swap nums on condition
-            // } else {
-            //     return -1;            // return -1 means it don't swap nums on condition
-            // }
-
-        }; */
+        Comparator<Students> com = (i, j) -> i.age > j.age ? 1 : -1; // we can use this type of Lamda Expression in one
+                                                                     // line or Below the code of both types
+        /*
+         * Comparator<Students> com = (i, j)
+         * -> {
+         * return i.age > j.age ? 1 : -1;
+         * 
+         * // 🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️ [Instead of if else we can Use Ternory
+         * Operator]
+         * // if (i.age > j.age) {
+         * // return 1; // return 1 means it swap nums on condition
+         * // } else {
+         * // return -1; // return -1 means it don't swap nums on condition
+         * // }
+         * 
+         * };
+         */
 
         List<Students> numsList = new ArrayList<>();
         numsList.add(new Students("Rahul", 21));
--- Basic+core/Collection/Stackk.java
+++ Basic+core/Collection/Stackk.java
@@ -9,8 +9,12 @@ public class Stackk {
         sk.add(102); // These Methods are deprecated OR
         sk.add(103); // It is Valid Because
         sk.add(104); // Vector Extends Stack So
-       
+        sk.add(106); // It have All VEctor Properties
+        sk.add(107); // But Recommendation is uSe push and pop }
+        sk.add(108);
+        sk.add(4, 105); // To add element at specific position
 
+        
         System.out.println(sk);
     }
 }
