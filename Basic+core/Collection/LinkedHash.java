4479bc67a9cd48ec83b8243cfd33912a81229210
Ajay <gangwar030@gmail.com>
2025-10-10 20:55:16 +0530

    LinkedHashSet

---
+++ Basic+core/Collection/LinkedHash.java
@@ -0,0 +1,25 @@
+import java.util.LinkedHashSet;
+
+// It Eliminate the duplicate value from the set
+// It is used to remove the duplicate value from the set
+// It is Not Provided Sorted value in the set.
+
+
+public class LinkedHash {
+    public static void main(String[] args) {
+        
+
+        LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
+
+        lhs.add(10);
+        lhs.add(20);
+        lhs.add(50);
+        lhs.add(20);
+        lhs.add(100);
+        lhs.add(500);
+        lhs.add(80);
+
+        System.out.println(lhs);
+
+    }
+}
