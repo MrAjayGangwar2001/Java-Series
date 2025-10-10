599e1a806d69acc53abc9ffa8259e539a44a7111
Ajay <gangwar030@gmail.com>
2025-10-11 00:32:06 +0530

    Vector

---
+++ Basic+core/Collection/Vectors.java
@@ -0,0 +1,17 @@
+import java.util.Enumeration;
+import java.util.Vector; 
+
+
+//  Vector is a Synchronized so it is slow and thread safe
+public class Vectors {
+    public static void main(String[] args) {
+        
+
+      Vector<String> vc = new Vector<String>();
+      vc.add("am");
+      vc.add("Ajay");
+      vc.add("Gangwar");
+     
+        System.out.println(vc);
+    }
+}
