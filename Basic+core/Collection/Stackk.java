da83d2b4798d219e07db80fd1320e44d38a92d88
Ajay <gangwar030@gmail.com>
2025-10-11 00:29:00 +0530

    Stack

--- Basic+core/Collection/Stackk.java
+++ Basic+core/Collection/Stackk.java
@@ -5,14 +5,14 @@ public class Stackk {
 
         Stack<Integer> sk = new Stack<>();
 
-        sk.add(101); {
-        sk.add(102); // These Methods are deprecated OR
-        sk.add(103); // It is Valid Because
-        sk.add(104); // Vector Extends Stack So
-        sk.add(106); // It have All VEctor Properties
-        sk.add(107); // But Recommendation is uSe push and pop }
-        sk.add(108);
-        sk.add(4, 105); // To add element at specific position
+        // sk.add(101); {
+        // sk.add(102); // These Methods are deprecated OR
+        // sk.add(103); // It is Valid Because
+        // sk.add(104); // Vector Extends Stack So
+        // sk.add(106); // It have All VEctor Properties
+        // sk.add(107); // But Recommendation is uSe push and pop }
+        // sk.add(108);
+        // sk.add(4, 105); // To add element at specific position
 
         sk.push(11);
         sk.push(12);
