4e508af4303f4174b21bd0d7927f7da21b59fb07
Ajay <gangwar030@gmail.com>
2025-10-10 21:02:21 +0530

    Sorting

--- Basic+core/Collection/Sorting.java
+++ Basic+core/Collection/Sorting.java
@@ -24,6 +24,35 @@ public class Sorting {
 
     public static void main(String[] args) {
         
+        // We are sorting numsList values basis on second or last digit like index 1
+        Comparator<Integer> com = new Comparator<Integer>()
+        {
+            public int compare(Integer i , Integer j)
+            {
+                if (i%10 > j%10) {
+                    return 1;             //  return 1 means it swap nums on condition
+                }
+                else {
+                    return -1;            // return -1 means it don't swap nums on condition
+                }
+            }
+        };
+        
+        List<Integer> numsList = new ArrayList<>();
+        numsList.add(51);
+        numsList.add(65);
+        numsList.add(79);
+        numsList.add(93);
+        numsList.add(37);
+        numsList.add(46);
+        numsList.add(52);
+
+        Collections.sort(numsList);
+        Collections.sort(numsList, com);
+
+        System.out.println(numsList);  */
+
+        
 
         List<Students> numsList = new ArrayList<>();
         numsList.add(new Students("Rahul", 21));
