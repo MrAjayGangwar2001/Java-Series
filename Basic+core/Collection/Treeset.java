8ff734a357727dafe1b5f9ca8bfaab7263947cf9
Ajay <gangwar030@gmail.com>
2025-10-13 23:33:18 +0530

    TreeSet

---
+++ Basic+core/Collection/Treeset.java
@@ -0,0 +1,30 @@
+import java.util.TreeSet;
+import java.util.Comparator;
+import java.util.Set;
+
+
+
+
+public class Treeset {
+    public static void main(String[] args) {
+
+        // Value Align in Assending Order
+
+        Set<Integer> ts = new TreeSet<>(new Comparator<>(){             // Using Comparator  ==> Ascending Order
+            public int compare(Integer a, Integer b){
+                return a-b;
+            }
+        });
+
+        
+
+        ts.add(10);
+        ts.add(20);
+        ts.add(50);
+        ts.add(30);
+        ts.add(40);
+       
+
+        System.out.println(ts);
+    }
+}
