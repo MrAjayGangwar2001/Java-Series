e71204f69759d0b8acf0861b7966aab8cb52e74c
Ajay <gangwar030@gmail.com>
2025-10-18 00:12:34 +0530

    ListInserAtStart

---
+++ Basic+core/DSA/List/insertAtStart.java
@@ -0,0 +1,10 @@
+// package List;
+// using System;
+//     public void insertAtStart(int data) {
+//         Node node = new Node();
+//         node.data = data;
+//         node.next = null;
+//         node.next = head;
+
+//         head = node;
+//     }
