5467b08fdbf45f7d3a4b66df782953f974aa6579
Ajay <gangwar030@gmail.com>
2025-10-18 00:05:44 +0530

    HashMap

--- Basic+core/Collection/Hashmap.java
+++ Basic+core/Collection/Hashmap.java
@@ -9,7 +9,18 @@ public class Hashmap {
         map.put(1, "Cat");
         map.put(3, "Bird");
         map.put(8, "Fish");
-       
+        map.put(66, "Elephant");
+        map.put(7, "Monkey");
+        map.put(4, "Lion");
+        map.put(5, "Tiger");
+        map.put(null, "Null");
+
+        System.out.println("HashMap Elements Are : " + map);
+
+        System.out.println("HashMap Size : " + map.size());
+        System.out.println("All Keys are : " + map.keySet());
+        System.out.println("All Values are : " + map.values());
+        System.out.println("All Entries are : " + map.entrySet());
         System.out.println("HashMap Contains Key 20 : " + map.containsKey(20));
         System.out.println("HashMap Contains Value Cat : " + map.containsValue("Cat"));
     }
