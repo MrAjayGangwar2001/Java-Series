c476cf927ca7f8731eb3574a6ed6ea33bd8508b1
Ajay <gangwar030@gmail.com>
2025-10-25 23:37:21 +0530

    AlienClass

---
+++ Springboot/FirsrApp/src/main/java/com/App/FirsrApp/Alien.java
@@ -0,0 +1,15 @@
+package com.App.FirsrApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+
+    @Autowired
+    Laptop lap;
+
+    public void Start() {
+        lap.coding();
+    }
+}
