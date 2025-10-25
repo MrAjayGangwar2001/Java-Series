f39c57ea54ba4c671d8267ba5a4952927d2ef0fb
Ajay <gangwar030@gmail.com>
2025-10-25 23:42:55 +0530

    LaptopClassFile

---
+++ Springboot/FirsrApp/src/main/java/com/App/FirsrApp/Laptop.java
@@ -0,0 +1,13 @@
+package com.App.FirsrApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void coding() {
+        System.out.println("I am Learning and Writing code in my laptop from Youtube");
+
+    }
+
+}
