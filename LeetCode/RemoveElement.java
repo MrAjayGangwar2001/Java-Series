f09d96ddcad85dfea75f83cdbf0a02d206e69db8
Ajay <gangwar030@gmail.com>
2025-11-28 23:15:12 +0530

    RemoveElement from Array

---
+++ LeetCode/RemoveElement.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+// Remove Specific Element from an Array
+public class RemoveElement{
+    public static int main(int[] args) {
+        int arr[] = {1,2,3,5,6,3,7,8,3,9,3,10};
+
+        int size = arr.length;
+        int target = 3;
+
+        for (int i = 0; i <=size;) {
+            // for (int j = i + 1; j <=size; j++) {
+                
+                if (arr[i] == target) {
+                    arr[i] = arr[i + 1];
+                    arr[size -1] = 0;
+                    size--;
+                }else{
+                    i++;
+                }
+            // }
+        }
+
+
+
+        return size;
+    }
+}
