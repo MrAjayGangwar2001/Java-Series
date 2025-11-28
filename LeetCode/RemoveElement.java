8556e1ff6561b5a9359d522d69b2d6d32a2df527
Ajay <gangwar030@gmail.com>
2025-11-29 01:03:19 +0530

    Problem Solved

--- LeetCode/RemoveElement.java
+++ LeetCode/RemoveElement.java
@@ -2,28 +2,35 @@ package LeetCode;
 
 // Remove Specific Element from an Array
 // Return Array Size After Removing Element
-public class RemoveElement{
-    public static int main(int[] args) {
-        int arr[] = {1,2,3,5,6,3,7,8,3,9,3,10};
+public class RemoveElement {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 5, 6, 3, 7, 8, 3, 9, 3, 10 };
 
         int size = arr.length;
+        System.out.println(size);
         int target = 3;
 
-        for (int i = 0; i <=size;) {
-            // for (int j = i + 1; j <=size; j++) {
-                
-                if (arr[i] == target) {
-                    arr[i] = arr[i + 1];
-                    arr[size -1] = 0;
-                    size--;
-                }else{
-                    i++;
+        for (int i = 0; i < size;) {
+
+            if (arr[i] == target) {
+                for (int j = i; j < size - 1; j++) {
+                    arr[j] = arr[j + 1];
+
                 }
-            // }
+                arr[size - 1] = 0;
+                size--;
+            } else {
+                i++;
+            }
+
+            // System.out.print(i + " ");
+
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
 
-
-
-        return size;
+        System.out.println("Size : " + size);
+        // return size;
     }
 }
